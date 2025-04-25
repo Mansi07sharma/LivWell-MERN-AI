92ab8bf6c609cd8c66eb78a39467d1f1e7f4af41
Mansi Sharma <yogesh11222211@gmail.com>
2025-04-25 21:29:47 +0530

    sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 
 import Navbar from "../Components/Navbar"
 import Footer from "../Components/Footer"
-import HeroSection from "../Components/HeroSection"
-import SearchBarSection from "../Components/RentPage/SearchBarSection"
+import Section1 from '../Components/Section1'
+import Section2 from '../Components/Section2'
+import Section3 from '../Components/Section3'
+import Section4 from '../Components/Section4'
+import Section5 from '../Components/Section5'
+import Section6 from '../Components/Section6'
 function App() {
 
   return (
     <>
-      {/* <Navbar />
-      <HeroSection />
-      <Footer /> */}
-      <SearchBarSection />
+      <div className="scroll-smooth">
+        <Navbar />
+        <Section1 />
+        <Section2 />
+        <Section3 />
+        <Section4 />
+        <Section5 />
+        <Section6 />
+        <Footer />
+      </div>
     </>
   )
 }
